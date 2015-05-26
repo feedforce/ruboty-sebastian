fac815e0449b4fd84ba3764987eb0c94102677b0
yukita <yukiyakonkon5@hotmail.co.jp>
2015-05-26 16:47:38 +0900

    /lib/ruboty/handlers/sebastian.rbのタイポを修正

--- lib/ruboty/handlers/sebastian.rb
+++ lib/ruboty/handlers/sebastian.rb
@@ -3,8 +3,8 @@ module Ruboty
     class Sebastian < Base
       # コマンドのパターン
       on /greet morning/, name: 'greet_at_morning', description: 'Output greeting message in the morning'
-      on /greet before close/, name: 'greet_before_closing', description: 'Output greeting message before close of buissness'
-      on /greet close/, name: 'greet_at_closing', description: 'Output greeting message for close of bissness'
+      on /greet before close/, name: 'greet_before_closing', description: 'Output greeting message before close of business'
+      on /greet close/, name: 'greet_at_closing', description: 'Output greeting message for close of business'
 
       # コマンドに対応したアクションを呼び出す
       def greet_at_morning(message)
