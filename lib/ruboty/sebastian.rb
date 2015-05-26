2c00e4077cf7c77dd2acd30628fe4c111aae23a0
yukita <yukiyakonkon5@hotmail.co.jp>
2015-05-26 15:18:07 +0900

    実装したMessageLoaderモジュールをrequireする記述をsebastian.rbに追加

--- lib/ruboty/sebastian.rb
+++ lib/ruboty/sebastian.rb
@@ -1,4 +1,5 @@
 require "ruboty/sebastian/version"
+require "ruboty/message_loader"
 require "ruboty/actions/greeting_at_morning"
 require "ruboty/actions/greeting_before_closing"
 require "ruboty/actions/greeting_at_closing"
