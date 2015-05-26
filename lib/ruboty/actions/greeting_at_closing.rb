c7e55a4d4a5b8c743b947d97070146057361a7ee
yukita <yukiyakonkon5@hotmail.co.jp>
2015-05-26 15:42:07 +0900

    GreetingAtClosing#callに金曜日の場合の発言をする処理を追加

--- lib/ruboty/actions/greeting_at_closing.rb
+++ lib/ruboty/actions/greeting_at_closing.rb
@@ -8,6 +8,7 @@ module Ruboty
 
         message.reply(messages.header[Random.new.rand(0..2)]) # 一言目
         message.reply(messages.middle[Random.new.rand(0..2)]) # 二言目
+        message.reply(messages.friday[Random.new.rand(0..1)]) if Time.now.friday?
       end
     end
   end
