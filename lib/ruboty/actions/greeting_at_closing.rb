829a007c43b54d9262b8e315c3e5adf1aff7a43f
yukita <yukiyakonkon5@hotmail.co.jp>
2015-05-26 17:10:02 +0900

    GreetingAtClosing#callの共通処理をメソッド化（GreetingAtClosing#reply_message）

--- lib/ruboty/actions/greeting_at_closing.rb
+++ lib/ruboty/actions/greeting_at_closing.rb
@@ -6,9 +6,14 @@ module Ruboty
       def call
         messages = load_message("greeting_at_closing")
 
-        message.reply(messages.header[Random.new.rand(0..2)]) # 一言目
-        message.reply(messages.middle[Random.new.rand(0..2)]) # 二言目
-        message.reply(messages.friday[Random.new.rand(0..1)]) if Time.now.friday?
+        reply_message(messages.header) # 一言目
+        reply_message(messages.middle) # 二言目
+        reply_message(messages.friday) if Time.now.friday? # 金曜日の場合
+      end
+
+      def reply_message(messages)
+        mes = messages[Random.new.rand(0...(messages.size))]
+        message.reply(mes) if mes 
       end
     end
   end
