ec71f7846579648950d9da7ddf1b7a0ec7ea232f
yukita <yukiyakonkon5@hotmail.co.jp>
2015-05-26 15:25:33 +0900

    greeting_at_closing.rbにMessageLoaderモジュールをincludeして, GreetingAtClosing#callでメッセージをロードする処理を追加

--- lib/ruboty/actions/greeting_at_closing.rb
+++ lib/ruboty/actions/greeting_at_closing.rb
@@ -1,7 +1,11 @@
 module Ruboty
   module Actions
     class GreetingAtClosing < Base
+      include MessageLoader
+
       def call
+        messages = load_message("greeting_at_closing")
+
         message.reply("hoge")
       end
     end
