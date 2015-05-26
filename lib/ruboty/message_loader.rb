b99cf376d7027175244e2716b25c1710d4266ff3
yukita <yukiyakonkon5@hotmail.co.jp>
2015-05-26 15:15:32 +0900

    settingslogicというgemを利用してymlファイルからメッセージをロードするMessageLoader#load_messageメソッドを実装

--- lib/ruboty/message_loader.rb
+++ lib/ruboty/message_loader.rb
@@ -1,2 +1,12 @@
+require 'settingslogic'
+
 module MessageLoader
+  class Settings < Settingslogic
+    source File.expand_path('../../settings.yml', File.dirname(__FILE__))
+    namespace "messages"
+  end
+
+  def load_message(type)
+    Settings.send(type)
+  end
 end
