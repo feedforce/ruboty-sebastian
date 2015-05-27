add8900778cc5276cceacde7d6e287d1ccb2fc5c
yukiyakonkon1024@gmail.com <Yukita@HidemiYukita-no-Mac-mini.local>
2015-05-27 17:01:41 +0900

    メッセージをキャッシュするGreetingAtMorning#greetingsを実装

--- lib/ruboty/actions/greeting_at_morning.rb
+++ lib/ruboty/actions/greeting_at_morning.rb
@@ -2,12 +2,14 @@ module Ruboty
   module Actions
     class GreetingAtMorning < Base
       def call
-        greetings = Sebastian::Settings.greeting_at_morning
-
         now  = Time.new
         week = ["日", "月", "火", "水", "木", "金", "土"]
 
         message.reply(sprintf(greetings.header, now.month, now.day, week[now.wday])) # 一言目
+      end
+
+      def greetings
+        @messages ||= Sebastian::Settings.greeting_at_morning
       end
     end
   end
