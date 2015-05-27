69e1d6989909121290ac257d54ac216a4d479218
otofu-square <yukiyakonkon1024@gmail.com>
2015-05-27 18:00:40 +0900

    GreetingAtMorning#callに東京ドーム、シティーホールの予定を発言する処理の追加（イベント取得ロジックは今後実装予定）

--- lib/ruboty/actions/greeting_at_morning.rb
+++ lib/ruboty/actions/greeting_at_morning.rb
@@ -11,6 +11,13 @@ module Ruboty
         else
           message.reply(sprintf(greetings.header, now.month, now.day, week[now.wday]))
         end
+
+        #️ 二言目
+        tokyodome_event = "" # 後日実装するメソッドからイベントを取得する
+        cityhall_event  = "" # 後日実装するメソッドからイベントを取得する
+
+        message.reply(sprintf(greetings.tokyodome.message, "【球団戦】", "球団1 - 球団2")) if tokyodome_event
+        message.reply(sprintf(greetings.cityhall.message,  "イベント名", "詳細URL")) if cityhall_event
       end
 
       def greetings
