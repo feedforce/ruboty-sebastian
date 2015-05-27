286442ed9cabc301e07f227b333d5a5250459416
yukiyakonkon1024@gmail.com <Yukita@HidemiYukita-no-Mac-mini.local>
2015-05-27 15:39:01 +0900

    GreetingAtClosingのリファクタリング

--- lib/ruboty/actions/greeting_at_closing.rb
+++ lib/ruboty/actions/greeting_at_closing.rb
@@ -2,14 +2,13 @@ module Ruboty
   module Actions
     class GreetingAtClosing < Base
       def call
-        messages = Sebastian::Settings.greeting_at_closing
-        reply_message(messages.header) # 一言目
-        reply_message(messages.middle) # 二言目
-        reply_message(messages.friday) if Time.now.friday? # 金曜日の場合
+        message.reply(messages.header.sample)
+        message.reply(messages.middle.sample)
+        message.reply(messages.friday.sample) if Time.now.friday? # 金曜日の場合
       end
 
-      def reply_message(messages)
-        message.reply(messages.sample)
+      def messages
+        @messages ||= Sebastian::Settings.greeting_at_closing
       end
     end
   end
