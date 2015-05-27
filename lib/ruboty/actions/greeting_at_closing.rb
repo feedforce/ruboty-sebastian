e9a866cc68659f3175d596d131554612d3c7f85d
yukiyakonkon1024@gmail.com <Yukita@HidemiYukita-no-Mac-mini.local>
2015-05-27 16:47:36 +0900

    混乱を避けるためGreetingAtClosing#messagesを#greetingsにリネーム

--- lib/ruboty/actions/greeting_at_closing.rb
+++ lib/ruboty/actions/greeting_at_closing.rb
@@ -2,13 +2,13 @@ module Ruboty
   module Actions
     class GreetingAtClosing < Base
       def call
-        message.reply(messages.header.sample)
-        message.reply(messages.middle.sample)
-        message.reply(messages.friday.sample) if Time.now.friday? # 金曜日の場合
+        message.reply(greetings.header.sample)
+        message.reply(greetings.middle.sample)
+        message.reply(greetings.friday.sample) if Time.now.friday? # 金曜日の場合
       end
 
-      def messages
-        @messages ||= Sebastian::Settings.greeting_at_closing
+      def greetings
+        @greetings ||= Sebastian::Settings.greeting_at_closing
       end
     end
   end
