6d5b6027bdc98bc71ee16aefc1222b5743a20a99
yukiyakonkon1024@gmail.com <Yukita@HidemiYukita-no-Mac-mini.local>
2015-05-27 17:10:59 +0900

    GreetingAtMorning#callに、1月1日の場合に年始の挨拶をする処理を追加

--- lib/ruboty/actions/greeting_at_morning.rb
+++ lib/ruboty/actions/greeting_at_morning.rb
@@ -2,10 +2,15 @@ module Ruboty
   module Actions
     class GreetingAtMorning < Base
       def call
-        now  = Time.new
+        now  = Time.now
         week = ["日", "月", "火", "水", "木", "金", "土"]
 
-        message.reply(sprintf(greetings.header, now.month, now.day, week[now.wday])) # 一言目
+        # 一言目
+        if now.month == 1 && now.day == 1
+          message.reply(sprintf(greetings.first_of_year.header, now.year))
+        else
+          message.reply(sprintf(greetings.header, now.month, now.day, week[now.wday]))
+        end
       end
 
       def greetings
