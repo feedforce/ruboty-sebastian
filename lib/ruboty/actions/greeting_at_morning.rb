d611645fdbe2f46522717228b132ce0b0710d796
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-03 12:06:34 +0900

    sprintfによる文字の埋め込みが見辛いため、%記法を使った処理に変更

--- lib/ruboty/actions/greeting_at_morning.rb
+++ lib/ruboty/actions/greeting_at_morning.rb
@@ -16,8 +16,8 @@ module Ruboty
         tokyodome_event = "" # 後日実装するメソッドからイベントを取得する
         cityhall_event  = "" # 後日実装するメソッドからイベントを取得する
 
-        message.reply(sprintf(greetings.tokyodome.message, "【球団戦】", "球団1 - 球団2")) if tokyodome_event
-        message.reply(sprintf(greetings.cityhall.message,  "イベント名", "詳細URL")) if cityhall_event
+        message.reply(greetings.tokyodome.message % ["【球団戦】", "球団1 - 球団2"]) if tokyodome_event
+        message.reply(greetings.cityhall.message  % ["イベント名", "詳細URL"]) if cityhall_event
 
         # 三言目
         if "実行年の最終出社日である"
