7a49f81a1f5190c2fc0382e4539d33aeeeca9eba
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-03 17:05:32 +0900

    空文字だとif文が通ってしまうため, nilに置き換え

--- lib/ruboty/actions/greeting_before_closing.rb
+++ lib/ruboty/actions/greeting_before_closing.rb
@@ -9,11 +9,11 @@ module Ruboty
         message.reply(greetings.middle)
 
         # 三言目
-        tokyodome_event = "" # 後日実装するメソッドからイベントを取得する
-        cityhall_event  = "" # 後日実装するメソッドからイベントを取得する
+        tokyodome_event = nil # 後日実装するメソッドからイベントを取得する
+        cityhall_event  = nil # 後日実装するメソッドからイベントを取得する
 
         message.reply(greetings.tokyodome.message % ["【球団戦】", "球団1 - 球団2"]) if tokyodome_event
-        message.reply(greetings.cityhall.message % ["イベント名", "詳細URL"]) if cityhall_event
+        message.reply(greetings.cityhall.message  % ["イベント名", "詳細URL"]) if cityhall_event
       end
 
       private
