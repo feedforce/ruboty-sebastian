e7ebe1d42963a7572c33febdcc29883a1cbcf73a
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-03 16:00:32 +0900

    シティホールのイベントページに接続して取得したHTMLをNokogiriで解析する処理を記述

--- lib/ruboty/actions/get_event.rb
+++ lib/ruboty/actions/get_event.rb
@@ -4,6 +4,13 @@ require 'nokogiri'
  
 module TokyoDomeEvent 
   def get_event_from_cityhall
+    uri = URI.parse("http://www.tokyo-dome.co.jp/tdc-hall/event/")
+ 
+    response = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.get(uri.path)
+    end
+ 
+    doc = Nokogiri::HTML.parse(response.body.force_encoding('UTF-8')) 
   end
  
   def get_event_from_dome
