5bbf2acbd292641bbd82c6a49920042403a97891
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-03 14:37:58 +0900

    sprintfを%メソッドに置き換え

--- lib/ruboty/actions/greeting_before_closing.rb
+++ lib/ruboty/actions/greeting_before_closing.rb
@@ -12,8 +12,8 @@ module Ruboty
         tokyodome_event = "" # 後日実装するメソッドからイベントを取得する
         cityhall_event  = "" # 後日実装するメソッドからイベントを取得する
 
-        message.reply(sprintf(greetings.tokyodome.message, "【球団戦】", "球団1 - 球団2")) if tokyodome_event
-        message.reply(sprintf(greetings.cityhall.message,  "イベント名", "詳細URL")) if cityhall_event
+        message.reply(greetings.tokyodome.message % ["【球団戦】", "球団1 - 球団2"]) if tokyodome_event
+        message.reply(greetings.cityhall.message % ["イベント名", "詳細URL"]) if cityhall_event
       end
 
       private
