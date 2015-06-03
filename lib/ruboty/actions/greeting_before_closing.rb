d48039a7b65310a2b101d7a06b20fc2a682194b7
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-03 11:50:04 +0900

    東京ドーム, シティホールの予定を発言する処理をGreetingBeforeClosing#callに追加

--- lib/ruboty/actions/greeting_before_closing.rb
+++ lib/ruboty/actions/greeting_before_closing.rb
@@ -7,6 +7,13 @@ module Ruboty
 
         # 二言目
         message.reply(greetings.middle)
+
+        # 三言目
+        tokyodome_event = "" # 後日実装するメソッドからイベントを取得する
+        cityhall_event  = "" # 後日実装するメソッドからイベントを取得する
+
+        message.reply(sprintf(greetings.tokyodome.message, "【球団戦】", "球団1 - 球団2")) if tokyodome_event
+        message.reply(sprintf(greetings.cityhall.message,  "イベント名", "詳細URL")) if cityhall_event
       end
 
       private
