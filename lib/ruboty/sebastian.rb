098124b07687d4982dc88ed84c35c57ed70f6bfa
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-10 09:58:31 +0900

    今回実装したget_event.rbを/lib/ruboty/sebastian.rbでインクルード

--- lib/ruboty/sebastian.rb
+++ lib/ruboty/sebastian.rb
@@ -2,6 +2,7 @@ require "ruboty/sebastian/version"
 require "ruboty/actions/greeting_at_morning"
 require "ruboty/actions/greeting_before_closing"
 require "ruboty/actions/greeting_at_closing"
+require "ruboty/actions/get_event"
 require "ruboty/handlers/sebastian"
 require "settingslogic"
 
