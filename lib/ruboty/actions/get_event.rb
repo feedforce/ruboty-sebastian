8e8dd96cfed2f40064254cf1461b5c10182f576f
otofu-square <yukiyakonkon1024@gmail.com>
2015-06-10 17:07:27 +0900

    css, xpathで検索する際にメソッドチェインを使った表記に使用し、値を取得する際の例外の発生を避けるよう修正

--- lib/ruboty/actions/get_event.rb
+++ lib/ruboty/actions/get_event.rb
@@ -6,13 +6,13 @@ module TokyoDomeEvent
     url = "http://www.tokyo-dome.co.jp/tdc-hall/event/"
     doc = Nokogiri::HTML.parse(open(url).read.force_encoding('UTF-8'))
 
-    column = doc.css("td[id='date_cell_%4d-%02d-%02d'] div" % [now.year, now.month, now.day])
+    column = doc.css("#date_cell_%4d-%02d-%02d" % [now.year, now.month, now.day]).search("a")
     return if column.empty?
 
     title = column.text.strip
 
     unless title.empty? || title == "【reserved】"
-      fragment = column.at("a").values[0].to_s
+      fragment = column.attribute("href").value
     else
       return
     end
@@ -24,12 +24,11 @@ module TokyoDomeEvent
     url = "http://www.tokyo-dome.co.jp/dome/schedule/"
     doc = Nokogiri::HTML.parse(open(url).read.force_encoding('UTF-8'))
 
-    column = doc.xpath("//th[contains(./text(), '#{now.day}日')]/following-sibling::*")
-    titles = column.xpath(".//p")
-    return if titles[0].nil? && titles[1].nil?
+    column = doc.xpath("//th[contains(./text(), '#{now.day}日')]/following-sibling::*").xpath(".//p")
+    return if column[0].nil? && column[1].nil?
 
-    title = titles[0].children.attribute('alt').value.strip
-    vs = titles[1].children.text.strip
+    title = column[0].children.attribute('alt').value.strip
+    vs = column[1].children.text.strip
 
     {title: title, vs: vs}
   end
